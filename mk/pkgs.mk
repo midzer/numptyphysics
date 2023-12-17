829c3e707dbb3579b6dbe1a0fa7090f63420c08e
Alexandre Detiste <alexandre.detiste@gmail.com>
2023-12-17 20:09:32 +0100

    make cross compile easier

--- mk/pkgs.mk
+++ mk/pkgs.mk
@@ -1,4 +1,6 @@
 ifneq ($(PKGS),)
-CXXFLAGS += $(shell $(CROSS_COMPILE)pkg-config --cflags $(PKGS))
-LIBS += $(shell $(CROSS_COMPILE)pkg-config --libs $(PKGS))
+PKG_CONFIG ?= $(CROSS_COMPILE)pkg-config
+
+CXXFLAGS += $(shell $(PKG_CONFIG) --cflags $(PKGS))
+LIBS += $(shell $(PKG_CONFIG) --libs $(PKGS))
 endif
